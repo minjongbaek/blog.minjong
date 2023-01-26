f6dcc7726b76ffcb8d7b7511d98566f19adddee3
minjongbaek <minjongbaek@gmail.com>
2023-01-26 17:15:49 +0900

    refector: 404페이지 스타일링

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,10 +2,13 @@ import { Link } from 'gatsby';
 
 const NotFoundPage = () => {
   return (
-    <>
-      <h1>페이지를 찾을 수 없습니다.</h1>
-      <Link to="/">홈으로</Link>
-    </>
+    <div className="flex flex-col h-screen justify-center items-center gap-2">
+      <h1 className="text-6xl">404</h1>
+      <h2 className="text-2xl">페이지를 찾을 수 없습니다.</h2>
+      <Link className="text-sky-500" to="/">
+        홈으로
+      </Link>
+    </div>
   );
 };
 
