256c48e8adf6dc352adc6cd17e99cec97eb184e3
minjongbaek <minjongbaek@gmail.com>
2023-05-08 01:42:39 +0900

    refactor: config 파일 ts로 변경

--- astro.config.ts
+++ astro.config.ts
@@ -2,16 +2,14 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
-
-// https://astro.build/config
 import react from "@astrojs/react";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.minjongdev.com",
+  site: import.meta.env.DEV
+    ? "http://localhost:3000"
+    : "https://blog.minjongdev.com",
   integrations: [
     mdx(),
     react(),
