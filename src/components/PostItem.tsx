b794f99d212de7328474cb23d9bdf2b235478315
minjongbaek <minjongbaek@gmail.com>
2023-05-12 23:55:09 +0900

    feat: about 페이지 작성

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -6,14 +6,11 @@ type PostItemProps = {
   post: CollectionEntry<"post">["data"];
 };
 
-const PostItem = ({
-  slug,
-  post: { title, date, tags, description, thumbnail },
-}: PostItemProps) => {
+const PostItem = ({ slug, post: { title, date, tags, description, thumbnail } }: PostItemProps) => {
   return (
     <a href={`/post/${slug}`} className="inline-block text-current group">
       <div className="flex flex-col sm:flex-row sm:gap-4 items-center border border-slate-300 sm:border-none p-4 sm:p-0">
-        <div className="bg-white sm:border border-slate-300 dark:border-none rounded-xl sm:w-48 w-full sm:h-48 h-32 flex-shrink-0 grid place-items-center">
+        <div className="bg-white sm:border border-slate-300 dark:border-none rounded-xl sm:w-48 w-full sm:h-48 h-32 flex-shrink-0 grid place-items-center select-none">
           <div className="text-black text-2xl text-center break-keep font-bold group-hover:text-blue-500 transition-colors duration-300">
             {thumbnail}
           </div>
@@ -24,9 +21,7 @@ const PostItem = ({
             {title}
           </h1>
           <p className="text-base">{description}</p>
-          <div className="text-sm text-slate-500">
-            {date.toLocaleDateString("ko-KR")}
-          </div>
+          <div className="text-sm text-slate-500">{date.toLocaleDateString("ko-KR")}</div>
         </div>
       </div>
     </a>
