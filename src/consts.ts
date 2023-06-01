7ca39dbeb9fd9c55d293bc85beba0e4227363479
minjong <48359052+minjongbaek@users.noreply.github.com>
2023-06-01 13:47:06 +0900

    검색 엔진 최적화 (#42)
    * feat: 메인, 이력서 페이지 빌드시 렌더링 하도록 수정
    
    * style: 아이콘 경로 수정
    
    * refactor: BaseHead를 Layout으로 부터 분리하고 slot으로 추가하도록 변경
    
    * style: 사이트 제목, 설명 수정
    
    * feat: robots 메타 태그 추가
    
    * style: keywords 메타 태그에 띄어쓰기 적용
    
    * style: 변경된 도메인으로 수정

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Blog.minjongdev";
-export const SITE_DESCRIPTION = "민종의 개발 블로그 입니다.";
+export const SITE_TITLE = "Blog.minjong";
+export const SITE_DESCRIPTION = "민종의 기록을 위한 블로그입니다.";
