747c643d9a55c41675c78eaf7cfe88d0c5998a54
minjongbaek <minjongbaek@gmail.com>
2023-06-18 20:14:48 +0900

    feat: vercel adapter 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import image from "@astrojs/image";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
+import vercelStatic from "@astrojs/vercel/static";
 
 const site = import.meta.env.DEV
   ? "http://localhost:8000"
@@ -37,4 +38,6 @@ export default defineConfig({
       theme: "dark-plus",
     },
   },
+  output: "static",
+  adapter: vercelStatic(),
 });
