a3e86c32728d52d6ebefd267d04a5a97d3bfd9af
minjong <48359052+minjongbaek@users.noreply.github.com>
2023-07-09 13:12:03 +0900

    스타일링 및 리팩터링 (#55)
    * refactor: Partytown 스크립트 컴포넌트화
    
    * setting: eslint 재설정
    
    * feat: Meta 타입 정의
    
    * refactor: 상수 utils/constants 로 위치 변경
    
    * refactor: BaseLayout 레이아웃 리팩터링
    
    * setting: 타입스크립트 설정
    
    * style: 블로그 메인 페이지 변경
    
    * refactor: 리액트 컴포넌트 제거
    
    * refactor: 컴포넌트들을 적절한 위치로 이동
    
    * feat: posts 페이지 추가
    
    * feat: meta title에 사이트 제목 추가
    
    * fix: 잘못된 import 경로 수정
    
    * chore: favicon 교체
    
    * chore: logo 교체

--- src/components/Post/TagItem.tsx
+++
@@ -1,9 +0,0 @@
-type TagItemProps = {
-  tag: string;
-};
-
-const TagItem = ({ tag }: TagItemProps) => {
-  return <span className="text-blue-600 dark:text-blue-500">#{tag}</span>;
-};
-
-export default TagItem;
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference path="astro/client" />
 /// <reference types="@astrojs/image/client" />
