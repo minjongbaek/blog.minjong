71ece28fe37617af9bda226e415b99002729922c
minjong <48359052+minjongbaek@users.noreply.github.com>
2023-07-10 11:52:17 +0900

    메인 페이지 하단에 프로젝트 표시 (#57)
    * refactor: Partytown 스크립트 컴포넌트화
    
    * setting: eslint 재설정
    
    * feat: Meta 타입 정의
    
    * refactor: 상수 utils/constants 로 위치 변경
    
    * refactor: BaseLayout 레이아웃 리팩터링
    
    * setting: 타입스크립트 설정
    
    * style: 블로그 메인 페이지 변경
    
    * refactor: 리액트 컴포넌트 제거
    
    * refactor: 컴포넌트들을 적절한 위치로 이동
    
    * feat: posts 페이지 추가
    
    * feat: meta title에 사이트 제목 추가
    
    * fix: 잘못된 import 경로 수정
    
    * chore: favicon 교체
    
    * chore: logo 교체
    
    * astro/image 컴포넌트 타입 설정
    
    * chore: 로고 이미지 교체
    
    * chore: 프로젝트 이미지 추가
    
    * feat: ProjectCard 컴포넌트 작성
    
    * refactor: 상대경로 import 절대 경로로 수정
    
    * refactor: utils 함수 위치 변경

---
+++ src/utils/constants/project.ts
@@ -0,0 +1,23 @@
+export const PROJECTS = [
+  {
+    title: "다독다독",
+    description: "책에 대한 인사이트를 공유하고 소통하는 독서 소셜 플랫폼",
+    tags: ["TypeScript", "Next.js", "React", "React-Query", "Chakra UI"],
+    imageURL: "/images/project/dadok-dadok.png",
+    githubURL: "https://github.com/prgrms-web-devcourse/Team-Gaerval-Dadok-FE",
+  },
+  {
+    title: "디그디그딥",
+    description: "커뮤니케이션을 통한 학습을 제공하는 개발자 전용 SNS",
+    tags: ["TypeScript", "React", "Recoil", "Styled-Components"],
+    imageURL: "/images/project/dig-dig-deep.png",
+    githubURL: "https://github.com/prgrms-fe-devcourse/FEDC3_DigDigDeep_Yuri",
+  },
+  {
+    title: "Blog.minjong.codes",
+    description: "기록을 위한 나만의 블로그",
+    tags: ["TypeScript", "Astro", "Tailwind CSS"],
+    imageURL: "/images/project/blog-minjong-codes.png",
+    githubURL: "https://github.com/minjongbaek/blog.minjong",
+  },
+];
