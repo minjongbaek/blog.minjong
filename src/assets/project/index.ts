49e7282faeb0ebf79e63c8ba67358ca24fac6ea5
minjongbaek <minjongbaek@gmail.com>
2023-09-06 20:53:32 +0900

    refactor: 프로젝트 이미지를 `/src/assets`로 이동

---
+++ src/assets/project/index.ts
@@ -0,0 +1,3 @@
+export { default as digdigProjectImage } from "./dig-dig-deep.png";
+export { default as dadokProjectImage } from "./dadok-dadok.png";
+export { default as blogProjectImage } from "./blog-minjong-codes.png";
--- src/utils/constants/project.ts
+++ src/utils/constants/project.ts
@@ -1,23 +1,29 @@
+import {
+  dadokProjectImage,
+  digdigProjectImage,
+  blogProjectImage,
+} from "@assets/project";
+
 export const PROJECTS = [
   {
     title: "다독다독",
     description: "책에 대한 인사이트를 공유하고 소통하는 독서 소셜 플랫폼",
     tags: ["TypeScript", "Next.js", "React", "React-Query", "Chakra UI"],
-    imageURL: "/images/project/dadok-dadok.png",
+    imageURL: dadokProjectImage,
     githubURL: "https://github.com/prgrms-web-devcourse/Team-Gaerval-Dadok-FE",
   },
   {
     title: "디그디그딥",
     description: "커뮤니케이션을 통한 학습을 제공하는 개발자 전용 SNS",
     tags: ["TypeScript", "React", "Recoil", "Styled-Components"],
-    imageURL: "/images/project/dig-dig-deep.png",
+    imageURL: digdigProjectImage,
     githubURL: "https://github.com/prgrms-fe-devcourse/FEDC3_DigDigDeep_Yuri",
   },
   {
     title: "Blog.minjong.codes",
     description: "기록을 위한 나만의 블로그",
     tags: ["TypeScript", "Astro", "Tailwind CSS"],
-    imageURL: "/images/project/blog-minjong-codes.png",
+    imageURL: blogProjectImage,
     githubURL: "https://github.com/minjongbaek/blog.minjong",
   },
 ];
