adf240001b678d83b0f04d5af69c85989fe35239
Shaw <minjongbaek@gmail.com>
2025-02-03 18:19:49 +0900

    feat: contents 디렉토리 내부에 있는 이미지를 public 디렉토리로 복사하는 스크립트 작성

---
+++ scripts/copy-images.mjs
@@ -0,0 +1,63 @@
+import fs from "fs";
+import path from "path";
+
+const CONTENTS_DIRECTORY = path.join(process.cwd(), "src/contents");
+const PUBLIC_IMAGES_DIRECTORY = path.join(
+  process.cwd(),
+  "public/images/content"
+);
+
+const copyImages = () => {
+  const nodes = fs.readdirSync(CONTENTS_DIRECTORY, { withFileTypes: true });
+  const contentTypes = nodes
+    .filter((node) => node.isDirectory())
+    .map((node) => node.name);
+
+  if (!fs.existsSync(PUBLIC_IMAGES_DIRECTORY)) {
+    fs.mkdirSync(PUBLIC_IMAGES_DIRECTORY, { recursive: true });
+  }
+
+  contentTypes.forEach((contentType) => {
+    const contentDirectories = fs
+      .readdirSync(path.join(CONTENTS_DIRECTORY, contentType), {
+        withFileTypes: true,
+      })
+      .filter((node) => node.isDirectory())
+      .map((node) => node.name);
+
+    contentDirectories.forEach((contentDirectory) => {
+      const sourceDirectory = path.join(
+        CONTENTS_DIRECTORY,
+        contentType,
+        contentDirectory
+      );
+
+      const targetDirectory = path.join(
+        PUBLIC_IMAGES_DIRECTORY,
+        contentType,
+        contentDirectory
+      );
+
+      const imageFiles = fs
+        .readdirSync(sourceDirectory)
+        .filter((fileName) => /.(png|gif)$/.test(fileName));
+
+      imageFiles.forEach((imageFile) => {
+        const sourcePath = path.join(sourceDirectory, imageFile);
+        const targetPath = path.join(targetDirectory, imageFile);
+
+        if (!fs.existsSync(targetDirectory)) {
+          fs.mkdirSync(targetDirectory, { recursive: true });
+        }
+
+        fs.copyFile(sourcePath, targetPath, (err) => {
+          if (err) {
+            console.error(err);
+          }
+        });
+      });
+    });
+  });
+};
+
+copyImages();
