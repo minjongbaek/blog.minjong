c2253a952926c8eda8cc66f81f609a01e512e6f4
Shaw <minjongbaek@gmail.com>
2025-03-05 14:32:14 +0900

    chore: 모든 패키지 업데이트

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:prettier/recommended",
+  ),
+  ...compat.plugins("prettier"),
+  {
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+];
+
+export default eslintConfig;
