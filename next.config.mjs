cd7b96d3769ceb2587af1c2c45eb9d86cd216c1c
minjongbaek <minjongbaek@gmail.com>
2025-03-09 11:48:52 +0900

    fix: eslint 적용되지 않는 현상 수정

--- next.config.mjs
+++ next.config.mjs
@@ -34,7 +34,7 @@ const nextConfig = {
   webpack: (config) => {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
+      rule.test?.test?.(".svg"),
     );
 
     config.module.rules.push(
@@ -50,7 +50,7 @@ const nextConfig = {
         issuer: fileLoaderRule.issuer,
         resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
         use: ["@svgr/webpack"],
-      }
+      },
     );
 
     // Modify the file loader rule to ignore *.svg, since we have it handled now.
