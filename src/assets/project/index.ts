4b3c9e397f954a5125f82f7e1a5a98413a7da336
minjongbaek <minjongbaek@gmail.com>
2025-04-10 20:25:11 +0900

    refactor: 사용하지 않는 파일 제거

--- src/assets/project/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as digdigProjectImage } from "./dig-dig-deep.png";
-export { default as dadokProjectImage } from "./dadok-dadok.png";
-export { default as blogProjectImage } from "./blog-minjong.png";
-export { default as shawKeeOsImage } from "./shaw-kee-os.png";
