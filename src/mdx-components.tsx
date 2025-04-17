f9849eac74dc2f04a59ec314b6b7fe371be27702
minjongbaek <minjongbaek@gmail.com>
2025-04-17 22:51:09 +0900

    feat: 마크다운에 포함된 이미지를 next/Image 컴포넌트로 렌더링 하도록 수정

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,17 @@
 import type { MDXComponents } from "mdx/types";
+import Image from "next/image";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    img: (props) => (
+      <Image
+        {...props}
+        alt={props.alt}
+        width={640}
+        height={400}
+        style={{ width: "100%", height: "auto" }}
+      />
+    ),
   };
 }
